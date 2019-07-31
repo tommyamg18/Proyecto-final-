9de5e844acfef354dae24b9450801cf6a57739e5
tommyamg18 <50183786+tommyamg18@users.noreply.github.com>
2019-07-31 16:39:08 -0400

    ajedrez faltan las stadisticas usuarios guardar partidas

--- Ajedrez/main.c
+++ Ajedrez/main.c
@@ -12,14 +12,3 @@ int main()
     menu();
 
 }
-
-//void intento()
-//{
-//    FILE* fichero;
-//    fichero = fopen("cursoAF1.txt", "wt");
-//    fputs("Aprender a programar (linea 1)\n", fichero);
-//    fputs("requiere esfuerzo (linea 2)\n", fichero);
-//    fputs("y dedicacion (linea 3)", fichero);
-//    fclose(fichero);
-//    printf("Proceso completado");
-//}
